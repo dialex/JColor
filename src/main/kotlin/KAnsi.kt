6a29ff13f6dda76555f2c9e7944d6c9f5bdc898f
Przemysław Frąckowiak-Szymański <7953131+dehydrogenaza@users.noreply.github.com>
2023-02-09 16:19:20 +0100

    Initial commit: added Kotlin extension functions to improve usability

---
+++ src/main/kotlin/KAnsi.kt
@@ -0,0 +1,8 @@
+import com.diogonunes.jcolor.*
+
+fun Command.colorize(): String = Ansi.colorize(this)
+
+infix fun String.colorize(ansiCode: String): String = Ansi.colorize(this, ansiCode)
+infix fun String.colorize(attributes: AnsiFormat): String = Ansi.colorize(this, attributes)
+fun String.colorize(vararg attributes: Attribute): String = Ansi.colorize(this, *attributes)
+infix fun String.colorize(attributes: Collection<Attribute>): String = Ansi.colorize(this, *attributes.toTypedArray())
